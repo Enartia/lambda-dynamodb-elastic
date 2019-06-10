e1692260034e4248f2c5d9f7edd490e9d4fed1d0
ggeorg0p <g.georgopoulos@usableweb.gr>
2019-06-10 08:26:20 +0300

    docs

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { pushStream } = require('dynamodb-stream-elasticsearch');
 
 const { ES_ENDPOINT, ES_INDEX, ES_TYPE } = process.env;
-
+/**
+ * Handles Lambda invocation
+ * 
+ * @param {AWS DynamoDB Streams Record Event} event The event object
+ * @param {AWS Lambda Context Object} context The context object
+ * @param {callback} callback The callback function
+ */
 function myHandler(event, context, callback) {
-    //  console.log('Received event:', JSON.stringify(event.Records));
+    // invoke pushstream from dynamodb-stream-elasticsearch passing event, endpoint, index, type params
     pushStream({ event, endpoint: ES_ENDPOINT, index: ES_INDEX, type: ES_TYPE })
         .then(() => {
             callback(null, `Successfully processed ${event.Records.length} records.`);
